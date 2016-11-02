7e030b06fa56d7e95b5f34365750fc60c74a7ccf
Scott Lewis <scott@iconify.it>
2016-11-02 00:31:37 -0400

    Replaced tabs with spaces in public template file

--- public/partials/iconfinder-portfolio-public-display.php
+++ public/partials/iconfinder-portfolio-public-display.php
@@ -13,19 +13,19 @@
  */
 ?>
 <div class="gs_drib_area gs_drib_theme1">
-	<div class="container">
-		<div class="row">
-			<?php foreach ($iconsets as $iconset) : ?>
-				<div class="col-md-3 drib-shots">
-				    <a href="<?php echo $iconset['permalink']; ?>" target="_blank">
-				    	<img src="<?php echo $iconset['preview']; ?>" alt="<?php echo $iconset['name']; ?> preview image" />
-				    </a>
-				    <p style="font-size: .8em;">
-				    	<a href="<?php echo $iconset['permalink']; ?>" target="_blank"><?php echo $iconset['name']; ?></a> 
-				    	<span style="float: right; margin-right: 10px;"><?php echo $iconset['price'] != "" ? "$" . $iconset['price'] : "Free"; ?></span>
-				    </p>
-				</div>
-			<?php endforeach; ?>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row">
+            <?php foreach ($iconsets as $iconset) : ?>
+                <div class="col-md-3 drib-shots">
+                    <a href="<?php echo $iconset['permalink']; ?>" target="_blank">
+                        <img src="<?php echo $iconset['preview']; ?>" alt="<?php echo $iconset['name']; ?> preview image" />
+                    </a>
+                    <p style="font-size: .8em;">
+                        <a href="<?php echo $iconset['permalink']; ?>" target="_blank"><?php echo $iconset['name']; ?></a> 
+                        <span style="float: right; margin-right: 10px;"><?php echo $iconset['price'] != "" ? "$" . $iconset['price'] : "Free"; ?></span>
+                    </p>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </div>
